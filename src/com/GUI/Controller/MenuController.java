62a510388e1f4476717321e7800f26793ea420b2
NBilal <bilalahmedim@gmail.com>
2019-05-07 16:27:57 +0530

    Navigation Change

--- src/com/GUI/Controller/MenuController.java
+++ src/com/GUI/Controller/MenuController.java
@@ -13,50 +13,70 @@ import java.io.IOException;
 
 public class MenuController {
   @FXML
+  private Button Home;
+  @FXML
   private Button Pass_Outs;
   @FXML
   private Button Fees;
   @FXML
   private Button New_Admission;
   @FXML
+  private Button Correction_Search;
+  @FXML
   private Button Result;
   @FXML
   private Button About;
   @FXML
+  private Button Close;
+  @FXML
   public void changeScene(ActionEvent event) throws IOException {
-    if (event.getSource() == Pass_Outs) {
+    if(event.getSource() == Home){
+      Parent Pane = FXMLLoader.load(getClass().getResource("../FXML/MainWindow.fxml"));
+      Scene scene = new Scene(Pane);
+      Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+      window.setTitle("Student Management System");
+      window.setScene(scene);
+      window.show();
+    }else if (event.getSource() == Pass_Outs) {
       Parent Pane = FXMLLoader.load(getClass().getResource("../FXML/PassOuts.fxml"));
       Scene scene = new Scene(Pane);
       Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-      window.setTitle("Pass Outs Students");
+      window.setTitle("Student Pass Outs");
       window.setScene(scene);
       window.show();
     } else if(event.getSource() == Fees){
-      Parent pane = FXMLLoader.load(getClass().getResource("../FXML/Fees.fxml"));
-      Scene scene = new Scene(pane);
+      Parent Pane = FXMLLoader.load(getClass().getResource("../FXML/Fees.fxml"));
+      Scene scene = new Scene(Pane);
       Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
       window.setTitle("Fees Submission");
       window.setScene(scene);
       window.show();
     }else if(event.getSource() == New_Admission){
-      Parent pane = FXMLLoader.load(getClass().getResource("../FXML/NewAdmission.fxml"));
-      Scene scene = new Scene(pane);
+      Parent Pane = FXMLLoader.load(getClass().getResource("../FXML/NewAdmission.fxml"));
+      Scene scene = new Scene(Pane);
       Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
       window.setTitle("New Admission");
       window.setScene(scene);
       window.show();
+    }else if(event.getSource() == Correction_Search){
+      Parent Pane = FXMLLoader.load(getClass().getResource("../FXML/Correction_Search.fxml"));
+      Scene scene = new Scene(Pane);
+      Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+      window.setTitle("Correction Search");
+      window.setScene(scene);
+      window.show();
     }else if(event.getSource() == Result){
-      Parent pane = FXMLLoader.load(getClass().getResource("../FXML/Result.fxml"));
-      Scene scene = new Scene(pane);
+      Parent Pane = FXMLLoader.load(getClass().getResource("../FXML/Result.fxml"));
+      Scene scene = new Scene(Pane);
       Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
       window.setTitle("Student Results");
       window.setScene(scene);
       window.show();
     }else if(event.getSource() == About){
-      Parent pane = FXMLLoader.load(getClass().getResource("../FXML/About.fxml"));
-      Scene scene = new Scene(pane);
+      Parent Pane = FXMLLoader.load(getClass().getResource("../FXML/About.fxml"));
+      Scene scene = new Scene(Pane);
       Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-      window.setTitle("About");
+      window.setTitle("About Us");
       window.setScene(scene);
       window.show();
     }
